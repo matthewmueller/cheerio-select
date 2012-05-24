6e36d5ca40457897c53a0a1217bb4b1d4a243264
Matt Mueller <mattmuelle@gmail.com>
2012-05-23 21:22:52 -0700

    class tests complete

--- test/helpers.js
+++ test/helpers.js
@@ -1,6 +1,30 @@
-var slice = Array.prototype.slice,
-    expect = require('expect.js');
+/**
+ * Module dependencies.
+ */
+var $ = require('../'),
+    _ = require('underscore'),
+    parse = require('cheerio').parse,
+    expect  = require('expect.js'),
+    helpers = require('./helpers'),
+    read = require('fs').readFileSync;
+
+/* 
+ * Load test data
+ */
+var html = read(__dirname + '/fixtures/index.html'),
+    dom  = parse(html);
+
+/*
+ * Helper classes
+ */
 
 exports.ids = function(elem) {
   return elem.attribs.id;
+};
+
+exports.t = function(description, selector, ids) {
+  it(description, function() {
+    var result = $(selector, dom).map(exports.ids);
+    expect(result).to.eql(ids);
+  });
 };
