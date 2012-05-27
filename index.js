e4be3e3dc684ca7ba164b907a9c608105a352965
Matt Mueller <mattmuelle@gmail.com>
2012-05-27 15:18:51 -0700

    use fs for node 0.4.x support

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 exports = module.exports = require('./lib/select');
 
 /*
- * Library version
- */
-exports.version = require('./package.json').version;
+  Export the version
+*/
+exports.version = (function() {
+  var pkg = require('fs').readFileSync(__dirname + '/package.json', 'utf8');
+  return JSON.parse(pkg).version;
+})();
